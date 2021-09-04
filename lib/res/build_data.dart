1ddb2f53f8391a5ad670ab969295f7dab5fa5b31
LollipopKit <2036293523@qq.com>
2021-09-04 14:42:09 +0800

    修复包名

--- lib/res/build_data.dart
+++ lib/res/build_data.dart
@@ -2,9 +2,8 @@
 
 class BuildData {
   static const String name = "Toast题库";
-  static const int build = 38;
-  static const String engine =
-      "Flutter 2.2.3 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision f4abaa0735 (7 weeks ago) • 2021-07-01 12:46:11 -0700\nEngine • revision 241c87ad80\nTools • Dart 2.13.4\n";
-  static const String buildAt = "2021-08-19 20:37:45.555069";
-  static const int modifications = 1;
+  static const int build = 40;
+  static const String engine = "Flutter 2.2.3 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision f4abaa0735 (9 weeks ago) • 2021-07-01 12:46:11 -0700\nEngine • revision 241c87ad80\nTools • Dart 2.13.4\n";
+  static const String buildAt = "2021-09-04 09:34:17.507782";
+  static const int modifications = 0;
 }
