ec6c4b48dc8b54cb0679dc528a8bc72ccb68f9eb
LollipopKit <2036293523@qq.com>
2021-10-07 20:29:10 +0800

    修改iOS splash

--- lib/res/build_data.dart
+++ lib/res/build_data.dart
@@ -2,8 +2,8 @@
 
 class BuildData {
   static const String name = "Toast题库";
-  static const int build = 55;
+  static const int build = 56;
   static const String engine = "Flutter 2.5.2 • channel stable • https://github.com/flutter/flutter.git\nFramework • revision 3595343e20 (6 days ago) • 2021-09-30 12:58:18 -0700\nEngine • revision 6ac856380f\nTools • Dart 2.14.3\n";
-  static const String buildAt = "2021-10-06 22:23:07.554452";
-  static const int modifications = 9;
+  static const String buildAt = "2021-10-06 22:34:10.496677";
+  static const int modifications = 1;
 }
